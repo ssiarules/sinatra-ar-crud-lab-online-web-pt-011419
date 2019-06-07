ae7ba23908494421db3a2da1f43de4226e2d2499
Johnson Joseph <jhonsonj88@gmail.com>
2019-06-07 04:10:24 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,50 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+
   end
+
+  get 'articles/new' do
+    erb :new
+  end
+
+post '/articles' do
+   article = Article.new({title: params[:title], content: params[:content]})
+   article.save
+
+   redirect to '/articles'
+ end
+
+ get '/articles' do
+  @articles=Article.all
+
+  erb :index
 end
+
+get '/articles/:id/edit' do
+ @article = Article.find(params[:id].to_i)
+erb :edit
+end
+
+patch '/articles/:id' do
+    article = Article.find(params[:id].to_i)
+    article.update(title: params[:title])
+    article.update(content: params[:content])
+    article.save
+
+    redirect to '/articles/'+article.id.to_s
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id].to_i)
+    erb :show
+  end
+
+  delete '/articles/:id/delete' do
+    article = Article.find(params[:id].to_i)
+    @title = article.title
+    article.destroy
+
+    erb :delete
+   end
+ end
--- db/migrate/20151201201114_create_articles.rb
+++ db/migrate/20151201201114_create_articles.rb
@@ -1,4 +1,6 @@
 class CreateArticles < ActiveRecord::Migration[5.1]
+  
+  
   def change
     create_table :articles do |t|
       t.string :title
